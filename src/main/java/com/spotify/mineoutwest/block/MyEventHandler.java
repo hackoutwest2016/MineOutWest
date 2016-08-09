e812dc07490008d082837dccb6b78b527351d76b
Niklas Gustavsson <niklas@protocol7.com>
2016-08-09 18:47:05 +0200

    YOLO

--- src/main/java/com/spotify/mineoutwest/block/MyEventHandler.java
+++ src/main/java/com/spotify/mineoutwest/block/MyEventHandler.java
@@ -5,22 +5,33 @@ import java.util.List;
 
 import com.spotify.mineoutwest.Sounds;
 import net.minecraft.util.SoundCategory;
+import net.minecraft.world.World;
 import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 public class MyEventHandler {
 
-  @SubscribeEvent
-  public void world(WorldEvent event) {
-    long worldTime = event.getWorld().getWorldTime();
+  private World world;
 
-    // check for valid event time (NPE hell otherwise)
-    if (worldTime > 0) {
-      List<Sounds.ScheduledSound> sounds = Sounds.getScheduledSound(worldTime);
-      for (Sounds.ScheduledSound sound : sounds) {
-        event.getWorld().playSound(null, sound.getX(), sound.getY(), sound.getZ(), sound.getEvent(), SoundCategory.MASTER, 2, 1);
-        System.out.println("Started sound " + sound + " at " + worldTime);
+  @SubscribeEvent
+  public void tick(TickEvent.ServerTickEvent event) {
+    if (world != null) {
+      long worldTime = world.getWorldTime();
+
+      // check for valid event time (NPE hell otherwise)
+      if (worldTime > 0) {
+        List<Sounds.ScheduledSound> sounds = Sounds.getScheduledSound(worldTime);
+        for (Sounds.ScheduledSound sound : sounds) {
+          world.playSound(null, sound.getX(), sound.getY(), sound.getZ(), sound.getEvent(), SoundCategory.MASTER, 2, 1);
+          System.out.println("Started sound " + sound + " at " + worldTime);
+        }
       }
     }
+  }
+
+    @SubscribeEvent
+  public void world(WorldEvent event) {
+    this.world = event.getWorld();
   }
 }
