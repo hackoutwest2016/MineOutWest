a4f93cfbc9769291533ba3b254f54fe265c6153d
Simon Hofverberg <shg@spotify.com>
2016-08-10 11:02:20 +0200

    Add helper funcs to Stage.

--- src/main/java/com/mineoutwest/Stages.java
+++ src/main/java/com/mineoutwest/Stages.java
@@ -3,10 +3,8 @@ package com.mineoutwest;
 import net.minecraft.entity.EntityCreature;
 import net.minecraft.entity.ai.EntityAIBase;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3i;
 
-/**
- * Created by Simon on 2016-08-09.
- */
 public class Stages {
 
     //public static final BlockPos LINNE = new BlockPos(75, 66, 335);
@@ -14,21 +12,64 @@ public class Stages {
     //public static final BlockPos FLAMINGO = new BlockPos(140, 65, 212);
     //public static final BlockPos DUNGEN = new BlockPos(161, 65, 43);
 
-    public static class Stage {
-        private String NAME;
-        private BlockPos CR;
-        private BlockPos FL;
-        private BlockPos FR;
-        private BlockPos BL;
-        private BlockPos BR;
+    public static class Area {
+        public BlockPos FL;
+        public BlockPos FR;
+        public BlockPos BL;
+        public BlockPos BR;
 
-        private BlockPos CROWD_FL;
-        private BlockPos CROWD_FR;
-        private BlockPos CROWD_BL;
-        private BlockPos CROWD_BR;
+        public Area( BlockPos front_left,
+                     BlockPos front_right,
+                     BlockPos back_left,
+                     BlockPos back_right) {
+            FL = front_left;
+            FR = front_right;
+            BL = back_left;
+            BR = back_right;
+        }
+
+        public BlockPos getMidPoint() {
+            BlockPos mid = new BlockPos( (FL.getX() + FR.getX() + BL.getX() + BR.getX()) / 4,
+                                         (FL.getY() + FR.getY() + BL.getY() + BR.getY()) / 4,
+                                         (FL.getZ() + FR.getZ() + BL.getZ() + BR.getZ()) / 4);
+            return mid;
+        }
+
+        public boolean isPosInArea(BlockPos pos) {
+            Vec3i p1 = new Vec3i(FL.getX(), 0.0, FL.getZ());
+            Vec3i p2 = new Vec3i(FR.getX(), 0.0, FR.getZ());
+            Vec3i p3 = new Vec3i(BR.getX(), 0.0, BR.getZ());
+            Vec3i p4 = new Vec3i(BL.getX(), 0.0, BL.getZ());
+
+            Vec3i L1 = new Vec3i( p2.getX() - p1.getX(), 0.0, p2.getZ() - p1.getZ());
+            Vec3i L2 = new Vec3i( p3.getX() - p2.getX(), 0.0, p3.getZ() - p2.getZ());
+            Vec3i L3 = new Vec3i( p4.getX() - p3.getX(), 0.0, p4.getZ() - p3.getZ());
+            Vec3i L4 = new Vec3i( p1.getX() - p4.getX(), 0.0, p1.getZ() - p4.getZ());
+
+            Vec3i P = new Vec3i(pos.getX(), 0.0, pos.getZ());
+            Vec3i P1 = new Vec3i( P.getX() - p1.getX(), 0.0, P.getZ() - p1.getZ());
+            Vec3i P2 = new Vec3i( P.getX() - p2.getX(), 0.0, P.getZ() - p2.getZ());
+            Vec3i P3 = new Vec3i( P.getX() - p3.getX(), 0.0, P.getZ() - p3.getZ());
+            Vec3i P4 = new Vec3i( P.getX() - p4.getX(), 0.0, P.getZ() - p4.getZ());
+
+
+            Vec3i C1 = P1.crossProduct(L1);
+            Vec3i C2 = P2.crossProduct(L2);
+            Vec3i C3 = P3.crossProduct(L3);
+            Vec3i C4 = P4.crossProduct(L4);
+
+            return (C1.getY() >= 0.0 && C2.getY() >= 0.0 && C3.getY() >= 0.0 && C4.getY() >= 0.0);
+        }
+    }
+
+
+    public static class Stage {
+        public String NAME;
+
+        public Area stage;
+        public Area crowd;
 
         public Stage(String name,
-            BlockPos center,
             BlockPos front_left,
             BlockPos front_right,
             BlockPos back_left,
@@ -38,32 +79,25 @@ public class Stages {
             BlockPos crowd_bl,
             BlockPos crowd_br) {
             NAME = name;
-            CR = center;
-            FL = front_left;
-            FR = front_right;
-            BL = back_left;
-            BR = back_right;
-            CROWD_FL = crowd_fl;
-            CROWD_FR = crowd_fr;
-            CROWD_BL = crowd_bl;
-            CROWD_BR = crowd_br;
+            stage = new Area(front_left, front_right, back_left, back_right);
+            crowd = new Area(crowd_fl, crowd_fr, crowd_bl, crowd_br);
         }
+
       public BlockPos getCenter() {
-        return this.CR;
+        return stage.getMidPoint();
       }
     }
 
     public static final BlockPos AZALEASTAGECR = new BlockPos(122, 66, 118);
-    private static final BlockPos AZALEASTAGEFL = new BlockPos(135, 68, 106);
-    private static final BlockPos AZALEASTAGEFR = new BlockPos(110, 68, 106);
-    private static final BlockPos AZALEASTAGEBL = new BlockPos(135, 68, 106);
-    private static final BlockPos AZALEASTAGEBR = new BlockPos(110, 68, 106);
-    private static final BlockPos AZALEACROWDFL = new BlockPos(110, 65, 118);
-    private static final BlockPos AZALEACROWDFR = new BlockPos(135, 65, 118);
-    private static final BlockPos AZALEACROWDBL = new BlockPos(110, 65, 138);
-    private static final BlockPos AZALEACROWDBR = new BlockPos(135, 65, 138);
+    public static final BlockPos AZALEASTAGEFL = new BlockPos(135, 68, 106);
+    public static final BlockPos AZALEASTAGEFR = new BlockPos(110, 68, 106);
+    public static final BlockPos AZALEASTAGEBL = new BlockPos(135, 68, 106);
+    public static final BlockPos AZALEASTAGEBR = new BlockPos(110, 68, 106);
+    public static final BlockPos AZALEACROWDFL = new BlockPos(110, 65, 118);
+    public static final BlockPos AZALEACROWDFR = new BlockPos(135, 65, 118);
+    public static final BlockPos AZALEACROWDBL = new BlockPos(110, 65, 138);
+    public static final BlockPos AZALEACROWDBR = new BlockPos(135, 65, 138);
     public static final Stage AZALEA = new Stage("AZALEA",
-        AZALEASTAGECR,
         AZALEASTAGEFL,
         AZALEASTAGEFR,
         AZALEASTAGEBL,
@@ -73,18 +107,17 @@ public class Stages {
         AZALEACROWDBL,
         AZALEACROWDBR);
 
-  private static final BlockPos FLAMINGOSTAGECR = new BlockPos(140, 65, 212);
+  public static final BlockPos FLAMINGOSTAGECR = new BlockPos(140, 65, 212);
   // todo update coorinates
-  private static final BlockPos FLAMINGOSTAGEFL = new BlockPos(132, 68, 219);
-  private static final BlockPos FLAMINGOSTAGEFR = new BlockPos(153, 68, 205);
-  private static final BlockPos FLAMINGOSTAGEBL = new BlockPos(132, 68, 231);
-  private static final BlockPos FLAMINGOSTAGEBR = new BlockPos(157, 68, 213);
-  private static final BlockPos FLAMINGOCROWDFL = new BlockPos(149, 65, 201);
-  private static final BlockPos FLAMINGOCROWDFR = new BlockPos(126, 65, 218);
-  private static final BlockPos FLAMINGOCROWDBL = new BlockPos(135, 65, 179);
-  private static final BlockPos FLAMINGOCROWDBR = new BlockPos(111, 65, 194);
+  public static final BlockPos FLAMINGOSTAGEFL = new BlockPos(132, 68, 219);
+  public static final BlockPos FLAMINGOSTAGEFR = new BlockPos(153, 68, 205);
+  public static final BlockPos FLAMINGOSTAGEBL = new BlockPos(132, 68, 231);
+  public static final BlockPos FLAMINGOSTAGEBR = new BlockPos(157, 68, 213);
+  public static final BlockPos FLAMINGOCROWDFL = new BlockPos(149, 65, 201);
+  public static final BlockPos FLAMINGOCROWDFR = new BlockPos(126, 65, 218);
+  public static final BlockPos FLAMINGOCROWDBL = new BlockPos(135, 65, 179);
+  public static final BlockPos FLAMINGOCROWDBR = new BlockPos(111, 65, 194);
   public static final Stage FLAMINGO = new Stage("FLAMINGO",
-      FLAMINGOSTAGECR,
       FLAMINGOSTAGEFL,
       FLAMINGOSTAGEFR,
       FLAMINGOSTAGEBL,
@@ -94,18 +127,17 @@ public class Stages {
       FLAMINGOCROWDBL,
       FLAMINGOCROWDBR);
 
-  private static final BlockPos LINNESTAGECR = new BlockPos(75, 66, 335);
+  public static final BlockPos LINNESTAGECR = new BlockPos(75, 66, 335);
   // todo update coorinates
-  private static final BlockPos LINNESTAGEFL = new BlockPos(99.065, 65, 119.145);
-  private static final BlockPos LINNESTAGEFR = new BlockPos(137.844, 65, 118.312);
-  private static final BlockPos LINNESTAGEBL = new BlockPos(84.564, 65, 145.904);
-  private static final BlockPos LINNESTAGEBR = new BlockPos(144.389, 65, 154.429);
-  private static final BlockPos LINNECROWDFL = new BlockPos(99.065, 65, 119.145);
-  private static final BlockPos LINNECROWDFR = new BlockPos(137.844, 65, 118.312);
-  private static final BlockPos LINNECROWDBL = new BlockPos(84.564, 65, 145.904);
-  private static final BlockPos LINNECROWDBR = new BlockPos(144.389, 65, 154.429);
+  public static final BlockPos LINNESTAGEFL = new BlockPos(99.065, 65, 119.145);
+  public static final BlockPos LINNESTAGEFR = new BlockPos(137.844, 65, 118.312);
+  public static final BlockPos LINNESTAGEBL = new BlockPos(84.564, 65, 145.904);
+  public static final BlockPos LINNESTAGEBR = new BlockPos(144.389, 65, 154.429);
+  public static final BlockPos LINNECROWDFL = new BlockPos(99.065, 65, 119.145);
+  public static final BlockPos LINNECROWDFR = new BlockPos(137.844, 65, 118.312);
+  public static final BlockPos LINNECROWDBL = new BlockPos(84.564, 65, 145.904);
+  public static final BlockPos LINNECROWDBR = new BlockPos(144.389, 65, 154.429);
   public static final Stage LINNE = new Stage("LINNE",
-      LINNESTAGECR,
       LINNESTAGEFL,
       LINNESTAGEFR,
       LINNESTAGEBL,
@@ -115,18 +147,17 @@ public class Stages {
       LINNECROWDBL,
       LINNECROWDBR);
 
-  private static final BlockPos DUNGENSTAGECR = new BlockPos(161, 65, 43);
+  public static final BlockPos DUNGENSTAGECR = new BlockPos(161, 65, 43);
   // todo update coorinates
-  private static final BlockPos DUNGENSTAGEFL = new BlockPos(99.065, 65, 119.145);
-  private static final BlockPos DUNGENSTAGEFR = new BlockPos(137.844, 65, 118.312);
-  private static final BlockPos DUNGENSTAGEBL = new BlockPos(84.564, 65, 145.904);
-  private static final BlockPos DUNGENSTAGEBR = new BlockPos(144.389, 65, 154.429);
-  private static final BlockPos DUNGENCROWDFL = new BlockPos(99.065, 65, 119.145);
-  private static final BlockPos DUNGENCROWDFR = new BlockPos(137.844, 65, 118.312);
-  private static final BlockPos DUNGENCROWDBL = new BlockPos(84.564, 65, 145.904);
-  private static final BlockPos DUNGENCROWDBR = new BlockPos(144.389, 65, 154.429);
+  public static final BlockPos DUNGENSTAGEFL = new BlockPos(99.065, 65, 119.145);
+  public static final BlockPos DUNGENSTAGEFR = new BlockPos(137.844, 65, 118.312);
+  public static final BlockPos DUNGENSTAGEBL = new BlockPos(84.564, 65, 145.904);
+  public static final BlockPos DUNGENSTAGEBR = new BlockPos(144.389, 65, 154.429);
+  public static final BlockPos DUNGENCROWDFL = new BlockPos(99.065, 65, 119.145);
+  public static final BlockPos DUNGENCROWDFR = new BlockPos(137.844, 65, 118.312);
+  public static final BlockPos DUNGENCROWDBL = new BlockPos(84.564, 65, 145.904);
+  public static final BlockPos DUNGENCROWDBR = new BlockPos(144.389, 65, 154.429);
   public static final Stage DUNGEN = new Stage("DUNGEN",
-      DUNGENSTAGECR,
       DUNGENSTAGEFL,
       DUNGENSTAGEFR,
       DUNGENSTAGEBL,
@@ -136,15 +167,4 @@ public class Stages {
       DUNGENCROWDBL,
       DUNGENCROWDBR);
 
-    //private static final BlockPos S1PFL = new BlockPos(99.065, 65, 119.145);
-//    private static final BlockPos S1PFR = new BlockPos(137.844, 65, 118.312);
-//    private static final BlockPos S1PBL = new BlockPos(84.564, 65, 145.904);
-//    private static final BlockPos S1PBR = new BlockPos(144.389, 65, 154.429);
-//    public static final Stage Stage1Pitch = new Stage(S1PFL, S1PFR, S1PBL, S1PBR);
-//
-//    private static final BlockPos S1FL = new BlockPos(136.7, 68, 117.8);
-//    private static final BlockPos S1FR = new BlockPos(99.3, 68, 117.4);
-//    private static final BlockPos S1BL = new BlockPos(136.7, 68, 106.3);
-//    private static final BlockPos S1BR = new BlockPos(99.3, 68, 106.3);
-//    public static final Stage Stage1 = new Stage(S1FL, S1FR, S1BL, S1BR);
 }
